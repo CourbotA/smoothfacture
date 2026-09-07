4ef691e701c370c61ab21988332394fd56b3fd3f
Courbot Antoine <72599332+CourbotA@users.noreply.github.com>
2026-09-07 10:15:29 -0500

    Route speech recognition through Expo Go compatibility layer

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -4,9 +4,19 @@ const { getDefaultConfig } = require('expo/metro-config');
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, '..');
 const config = getDefaultConfig(projectRoot);
+const speechCompatPath = path.resolve(projectRoot, 'src/services/speechRecognitionCompat.js');
 
 config.watchFolders = [workspaceRoot];
 config.resolver.nodeModulesPaths = [path.resolve(projectRoot, 'node_modules')];
 config.resolver.disableHierarchicalLookup = true;
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === 'expo-speech-recognition') {
+    return {
+      filePath: speechCompatPath,
+      type: 'sourceFile'
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = config;
