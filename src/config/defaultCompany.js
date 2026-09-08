aea796147adbd50c11e7eb940976bc3346e8191a
Courbot Antoine <72599332+CourbotA@users.noreply.github.com>
2026-09-08 15:03:17 -0500

    Add reform metadata to company profile

--- src/config/defaultCompany.js
+++ src/config/defaultCompany.js
@@ -20,8 +20,23 @@ export const DEFAULT_COMPANY_PROFILE = Object.freeze({
   },
   tax: {
     vatRegime: 'exempt_293b',
+    vatLiability: 'exempt_293b',
+    filingRegime: 'franchise_293b',
+    vatOnDebits: null,
     defaultVatRate: 0,
-    exemptionReason: 'TVA non applicable, art. 293 B du CGI'
+    vatNumber: '',
+    exemptionReason: 'TVA non applicable, art. 293 B du CGI',
+    specialRegime: null
+  },
+  reform: {
+    companySizeCategory: 'micro',
+    establishedInFrance: true,
+    supportsInternational: false,
+    chorusProEnabled: false,
+    vatGroup: false,
+    fiscalRepresentative: false,
+    selfBilling: false,
+    paConnection: null
   },
   payment: {
     iban: 'FR76 1670 6000 7101 3972 9000 019',
